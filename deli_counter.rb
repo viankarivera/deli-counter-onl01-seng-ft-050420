6f20c61de2a72f7fbb0ce0cf021dbda73241933c
Vianka Rivera <viankalrivera@gmail.com>
2020-05-07 22:10:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,6 +26,8 @@ end
 def now_serving(katz_deli)
 #if line is empty
 #then print out that the line is empty
-  katz_deli.length
+   if katz_deli.length == 0
   puts "There is nobody waiting to be served!"
+else 
+  
 end
