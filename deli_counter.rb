ee7e5480d4b0859585be85d9efcb44e8cf10396a
Vianka Rivera <viankalrivera@gmail.com>
2020-05-07 21:53:15 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+require "pry"
+
+def line(katz_deli)
+  counter = 1
+  current_line = "The line is currently:"
+
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    katz_deli.each do |name|
+      current_line << " #{counter}. #{name}"
+      counter += 1
+    end
+    puts current_line
+ #loop over katz_deli array and print out each person's place in line
+  end
+end
